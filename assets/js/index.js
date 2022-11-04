6f034df5bfa8f8aef865960d3cd7ab4e1e2110f3
Sami DEMIR <96701580+159SAMSPAROW@users.noreply.github.com>
2022-11-04 19:36:50 +0100

    update locomotive scroll

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,18 @@
 const scroll = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
+    inertia: 0.8,
     smooth: true,
-    tablet: { smooth: true },
-    mobile: { smooth: true },
+    getDirection: true,
+    mobile: {
+      smooth: true,
+      inertia: 0.8,
+      getDirection: true,
+    },
+    tablet: {
+      smooth: true,
+      inertia: 0.8,
+      getDirection: true,
+    },
   })
   
   scroll.on('scroll', () => {
