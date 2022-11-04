a22f33dccc9a29548dc817ce74e2edc3da0a856e
Sami DEMIR <96701580+159SAMSPAROW@users.noreply.github.com>
2022-11-04 16:01:27 +0100

    firt commit

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true,
+    tablet: { smooth: true },
+    smartphone: { smooth: true },
+  })
+  
+  scroll.on('scroll', () => {
+    if (document.querySelector('#color.is-inview')) {
+      document.body.style.background = 'rgb(163, 210, 251)'
+      document.body.style.background = 'rgb(65, 65, 165)'
+    } else {
+      document.body.style.background = 'rgb(65, 65, 165)'
+      document.body.style.background = 'rgb(163, 210, 251)'
+  }
+  })
+  
